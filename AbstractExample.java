5726fdcfa3360cc438081005289b8ec4baa0462e
monsterixy <108117899+monsterixy@users.noreply.github.com>
2022-06-24 09:36:55 +0200

    Add files via upload

---
+++ AbstractExample.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.eksoc.sniegula.interfaceexample;
+
+/**
+ *
+ * @author sniegulaa
+ */
+public abstract class AbstractExample {
+
+   private String attrbuteA;
+
+   public abstract void someAbstractMethod();
+    
+}
